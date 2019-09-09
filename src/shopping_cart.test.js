e17d9796fabacd79ee08d899758ce7a6eb4004ba
Sebastien Boutet <sboutet@sequans.com>
2019-09-09 17:30:52 +0200

    step 5

--- src/shopping_cart.test.js
+++ src/shopping_cart.test.js
@@ -1,23 +1,24 @@
 const ShoppingCart = require('./shopping_cart');
 
+let shoppingCart;
+beforeEach(() => {
+  shoppingCart = new ShoppingCart();
+});
+
 test('can create a shopping cart', () => {
-  let shoppingCart = new ShoppingCart();
   expect(shoppingCart).toBeInstanceOf(ShoppingCart);
 });
 
 test('can add an article to the shopping cart', () => {
-  let shoppingCart = new ShoppingCart();
   let article = {name:'socket', price: 1234};
   shoppingCart.add(article);
 });
 
 test('shopping cart must be empty at start', () => {
-  let shoppingCart = new ShoppingCart();
   expect(shoppingCart.articles).toHaveLength(0);
 });
 
 test('can retrieve added articles from the shopping cart', () => {
-  let shoppingCart = new ShoppingCart();
   let article1 = {name:'socket', price: 1234};
   let article2 = {name:'skirt', price: 4561};
   shoppingCart.add(article1);
