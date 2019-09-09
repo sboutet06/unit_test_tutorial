1a5ba32ae3d6586f84ca063ec4285f825dc086ee
Sebastien Boutet <sboutet@sequans.com>
2019-09-09 17:29:14 +0200

    step 2

--- src/shopping_cart.js
+++ src/shopping_cart.js
@@ -1,5 +1,7 @@
 class ShoppingCart{
 
+  add(article){        	   
+  }
 
 }
 
--- src/shopping_cart.test.js
+++ src/shopping_cart.test.js
@@ -4,3 +4,9 @@ test('can create a shopping cart', () => {
   let shoppingCart = new ShoppingCart();
   expect(shoppingCart).toBeInstanceOf(ShoppingCart);
 });
+
+test('can add an article to the shopping cart', () => {
+  let shoppingCart = new ShoppingCart();
+  let article = {name:'socket', price: 1234};
+  shoppingCart.add(article);
+});
