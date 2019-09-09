5bad6a31fe0f7643f898bb90b81e3910478a1b49
Sebastien Boutet <sboutet@sequans.com>
2019-09-09 17:28:13 +0200

    step 1

--- src/shopping_cart.js
+++ src/shopping_cart.js
@@ -1,1 +1,6 @@
+class ShoppingCart{
 
+
+}
+
+module.exports = ShoppingCart;
--- src/shopping_cart.test.js
+++ src/shopping_cart.test.js
@@ -1,3 +1,5 @@
+const ShoppingCart = require('./shopping_cart');
+
 test('can create a shopping cart', () => {
   let shoppingCart = new ShoppingCart();
   expect(shoppingCart).toBeInstanceOf(ShoppingCart);
